190da33bbe5ebb4dfaf0d4bc60d92bd04edc9b3e
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-10-24 08:03:35 +0500

    init DirectoryTree, ListItem

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DirectoryTree msg="Directory List"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DirectoryTree from './components/DirectoryTree.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DirectoryTree
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
